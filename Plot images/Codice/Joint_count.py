5732e4bf59459b0e1a5cac6f01b715461bbb4868
forgins <123165063+forgins@users.noreply.github.com>
2023-11-14 14:04:12 +0100

    Delete Plot images/Codice directory

--- Plot images/Codice/Joint_count.py
+++
@@ -1,7 +0,0 @@
-with open ('Punti_tuta.txt', 'r') as file:
-    punti = file.read().split(',')
-    print(punti)
-    num_punti_sporchi = len(punti)
-    punti_finali_xyz = num_punti_sporchi - 4 
-    print(punti_finali_xyz)
-    print(punti_finali_xyz/3)
